970d0239cdb4b3e653e0eb6002bf82ba8ee8a6e6
satoshi <uglysatoshi@ya.ru>
2024-06-07 23:06:40 +0300

    fixed login routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import {Box, Button, Checkbox, Container, Flex, Heading, Input, Link, useColorModeValue} from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 const IndexPage = () => {
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/main').then(() => 404);
+  }
+
   return (
       <Container maxW={"container.xl"} boxShadow={"base"} height={"100vh"} rounded={10} >
         <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"} >
@@ -10,10 +18,8 @@ const IndexPage = () => {
               <Input placeholder={"example@mail.com"} variant={"outline"} mb={3} type={"email"}/>
               <Input placeholder={"*******"} variant={"outline"} mb={6} type={"password"}/>
               <Checkbox mb={3} colorScheme={"gray"}>Remember me?</Checkbox>
-              <Button colorScheme={"gray"} mb={3}>
-                <Link href={"/main"}>
-                  Submit
-                </Link>
+              <Button colorScheme={"gray"} mb={3} onClick={handleClick}>
+                Submit
               </Button>
             </Flex>
           </Box>
